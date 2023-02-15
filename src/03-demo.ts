34f7619085e7cc7b17c1c22784a82d6d8abf0681
Kevin Parra <kevinparra535@gmail.com>
2023-02-14 19:35:47 -0500

    [ADD] TIPOS INFERIDOS

---
+++ src/03-demo.ts
@@ -0,0 +1,6 @@
+(() => {
+  let productName: string = "Producto 1";
+  let productPrice: number = 1_000;
+
+  productName.toLocaleUpperCase();
+})();
---
+++ src/03-typing.ts
@@ -0,0 +1,4 @@
+let productName: string = "Producto 1";
+let productPrice: number = 1_000;
+
+productName.toLocaleUpperCase()
